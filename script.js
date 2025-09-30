06e1773bd28204aff17f27c462cfb3f70b790ef8
dadada <alumno.652683@ies-azarquiel.es>
2025-09-30 10:02:35 +0200

    page

---
+++ script.js
@@ -0,0 +1,14 @@
+function filtrarTabla() {
+    let input = document.getElementById("filtro");
+    let filter = input.value.toUpperCase();
+    let table = document.getElementById("tablaSeguros");
+    let tr = table.getElementsByTagName("tr");
+
+    for (let i = 1; i < tr.length; i++) {
+        let td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            let txtValue = td.textContent || td.innerText;
+            tr[i].style.display = txtValue.toUpperCase().indexOf(filter) > -1 ? "" : "none";
+        }
+    }
+}
